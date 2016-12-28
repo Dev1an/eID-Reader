16dc1608c08139573f16a9955fc555e5117bc846
Damiaan Dufaux <damiaan@dpro.be>
2016-12-28 14:04:03 +0100

    Initial Commit

---
+++ cardreader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  cardreader
+//
+//  Created by Damiaan on 28-12-16.
+//  Copyright © 2016 Damiaan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ cardreader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  cardreader
+//
+//  Created by Damiaan on 28-12-16.
+//  Copyright © 2016 Damiaan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
